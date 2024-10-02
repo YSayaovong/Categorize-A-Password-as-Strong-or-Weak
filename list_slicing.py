4a3d8fc59b1aa958343c02ab60be3a0b83b08be5
Yengkong Sayaovong <110697608+YSayaovong@users.noreply.github.com>
2024-10-02 08:40:19 -0500

    Add files via upload

---
+++ list_slicing.py
@@ -0,0 +1,13 @@
+# List slicing
+amazon_cart = [
+ 'notebooks',
+ 'sunglasses',
+ 'toys',
+ 'grapes'
+]
+
+amazon_cart = 'laptop'
+new_cart = amazon_cart
+new_cart = 'gum'
+print(new_cart)
+print(amazon_cart)
